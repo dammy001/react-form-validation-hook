70ae4ba890106146e61ce1b542d672e313424980
Damilare Anjorin <damilareanjorin1@gmail.com>
2021-11-13 13:32:24 +0100

    fix

---
+++ rollup.config.js
@@ -0,0 +1,61 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable import/no-anonymous-default-export */
+import styles from 'rollup-plugin-styles';
+import { terser } from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel';
+import typescript from 'rollup-plugin-typescript2';
+import sourcemaps from 'rollup-plugin-sourcemaps';
+import commonjs from 'rollup-plugin-commonjs';
+const autoprefixer = require('autoprefixer');
+
+var MODE = [
+ {
+  format: 'cjs',
+ },
+ {
+  format: 'esm',
+ },
+ {
+  format: 'umd',
+ },
+];
+
+let config = [];
+
+MODE.map((m) => {
+ const conf = {
+  input: 'src/index.ts',
+  output: {
+   name: 'react-form-validation',
+   file: `dist/index.${m.format}.js`,
+   format: m.format,
+   exports: 'auto',
+  },
+  // this externalizes react to prevent rollup from compiling it
+  external: ['react', /@babel\/runtime/],
+  plugins: [
+   typescript({ useTsconfigDeclarationDir: true }),
+   babel({
+    exclude: 'node_modules/**',
+    plugins: ['@babel/transform-runtime'],
+    babelHelpers: 'runtime',
+   }),
+   styles({
+    postcss: {
+     plugins: [autoprefixer()],
+    },
+   }),
+   terser({
+    module: true,
+    output: {
+     comments: 'all',
+    },
+   }),
+   commonjs(),
+   sourcemaps(),
+  ],
+ };
+ config.push(conf);
+});
+
+export default [...config];
